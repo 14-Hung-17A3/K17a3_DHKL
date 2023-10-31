dc065fe8858ccabbaa7a05bf3649b65490af9e0e
14-Hung-17A3 <148058664+14-Hung-17A3@users.noreply.github.com>
2023-10-31 15:24:28 +0700

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,2 @@
+#Đây là chương trình đầu tiên
+print("Hello lớp K17A3 KHDL, 2023")
---
+++ tinhtoan_ver_1.py
@@ -0,0 +1,12 @@
+#Chương trình thứ hai
+#Tính toán số học nào
+x=10
+y=5
+tong=x+y
+hieu=5
+tich=x*y
+thuong=x/y
+print('Tổng của hai số là',x, '+', y, '=', tong)
+print('Hiệu của hai số là',x, '-', y, '=', hieu)
+print('Tích của hai số là',x, '*',y, '=', tich)
+print('Thương của hai số là', x,'/',y, '=', thuong)
